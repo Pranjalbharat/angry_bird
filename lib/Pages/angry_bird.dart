fed5db200727df2cd9c0c04af3b19cbfc38eb584
Pranjal <pranjalbharat07@gmail.com>
2024-02-12 21:44:01 +0530

    added

--- lib/Pages/angry_bird.dart
+++ lib/Pages/angry_bird.dart
@@ -1,10 +1,11 @@
 
 import 'dart:ui';
+import 'package:angry_bird/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/sprite.dart';
 
-class AngryBird extends SpriteComponent with DragCallbacks{
+class AngryBird extends SpriteComponent with HasGameRef<AngryBirdGame> {
 AngryBird() {
     // Call the onLoad function to initialize the sprite
     onLoad();
@@ -20,12 +21,30 @@ AngryBird() {
     position = Vector2(100, 300); // Set initial position
   }
 
+  void move(Vector2 position){
+    position.add(position);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if(isthrow){
+      move(Vector2(velocityX,velocityY));
+    }
+
+  }
+  double velocityX=0;
+  double velocityY=0;
+  bool isthrow=false;
+  // ignore: non_constant_identifier_names
+  void Throw(Vector2 coordinates,double velocity){
+    isthrow=true;
+    velocityX=coordinates.x;
+    velocityY=coordinates.y;
+    
 
 
-@override
-  void onDragUpdate(DragUpdateEvent event){ 
-}
-
+  }
 
 } 
 
--- lib/game.dart
+++ lib/game.dart
@@ -1,20 +1,42 @@
 import 'package:angry_bird/Pages/TargetPyramid.dart';
 import 'package:angry_bird/Pages/angry_bird.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/src/components/core/component.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
-class AngryBirdGame extends FlameGame {
+class AngryBirdGame extends FlameGame with PanDetector {
+late AngryBird angryBird;
+  TextComponent t1 = TextComponent(
+    text: 'Direction : 0',
+  );
+    TextComponent velocityText = TextComponent(
+    text: 'Velocity: 0',
+  );
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-
+  angryBird=AngryBird();
+    
     add(
-    AngryBird()
+    angryBird
     );
     add(
       TargetPyramid(this)
     );
+    add(t1..position = Vector2(size.x / 2, 0));
+      add(velocityText..position = Vector2(size.x / 2, 40));
   }
+
+    @override
+      void onPanUpdate(DragUpdateInfo info) {
+      double velocity = info.delta.global.length;
+    velocityText.text = 'Velocity: $velocity'; 
+      Vector2 direction = info.delta.global;
+      t1.text = "value : $direction"; 
+      angryBird.move(direction);
+  }
+
 
 }
